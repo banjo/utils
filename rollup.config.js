16234bfab5f06803f6be506756c410a8ff2606b3
Anton Ödman <anton.odman@gmail.com>
2023-01-22 20:20:00 +0100

    chore: unbuild -> rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import ts from "rollup-plugin-ts";
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "dist/index.js",
+                format: "cjs",
+            },
+            {
+                file: "dist/index.mjs",
+                format: "esm",
+            },
+        ],
+        plugins: [nodeResolve(), ts()],
+    },
+];
