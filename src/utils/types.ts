9246cc0b6bdad5977ee8239301774e0cb649dbb8
Anton Ödman <anton.odman@gmail.com>
2023-03-12 21:58:50 +0100

    chore: add docs to types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,12 +2,37 @@
  * Common TypeScript types.
  */
 
+/**
+ * A type that represents a function that returns a value of type T or undefined.
+ */
 export type Maybe<T> = T | undefined;
+/**
+ * A type that represents a function that returns a value of type T or null.
+ */
 export type Nullable<T> = T | null;
 
-export type Callback<T> = () => T;
-export type CallbackWithArgs<T, U> = (arg: T) => U;
+/**
+ * A type that represents a function that returns a value of type T or void.
+ */
+export type Callback<T = void> = () => T;
+/**
+ * A type that represents a callback with an argument of type T and returns U.
+ */
+export type CallbackWithArgs<T, U = void> = (arg: T) => U;
+/**
+ * A type that represents an async function that returns a value of type T or void.
+ */
 export type AsyncCallback<T> = () => Promise<T>;
+/**
+ * A type that represents an async callback with an argument of type T and returns U.
+ */
+export type AsyncCallbackWithArgs<T, U> = (arg: T) => Promise<U>;
+/**
+ * A type that represents a predicate function that returns a boolean.
+ */
 export type Predicate<T> = (value: T) => boolean;
 
+/**
+ * A type that represents a primitive value.
+ */
 export type Primitive = string | number | boolean | symbol | null | undefined;
